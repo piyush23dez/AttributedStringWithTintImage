7baae252c50d55258d67336fac0631b93b6f06fd
Piyush Sharma <piyush_sharma2@apple.com>
2017-02-21 16:55:01 -0800

    added image utility class for getting attributed string to append image at start or end commit

---
+++ TintColorStringWIthImage/ImageUtility.swift
@@ -0,0 +1,68 @@
+//
+//  ImageUtility.swift
+//  TintColorStringWIthImage
+//
+//  Created by Piyush Sharma on 2/21/17.
+//  Copyright © 2017 Piyush Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ImageUtility {
+    
+    enum ImagePosition {
+        case start, end
+    }
+    
+    static func createAttributedString(text: String, image: UIImage, imagePos: ImagePosition, imageColor: UIColor) -> NSMutableAttributedString {
+        
+        var range = NSMakeRange(0, 0)
+        let attachment = NSTextAttachment()
+        
+        attachment.bounds = CGRect(origin: CGPoint(x: 0, y: -2), size: CGSize(width: 15, height: 15))
+        
+        // image must be set to Template rendering mode
+        attachment.image = image
+        
+        //create a string with image attachment
+        let attachmentString = NSAttributedString(attachment: attachment)
+        
+        //create a title NSMutableAttributedString
+        let title = NSMutableAttributedString(string: text)
+        
+        // Need to use a space, or this bug/feature isn't activated
+        let fullString = NSMutableAttributedString(string: " ")
+        
+        switch imagePos {
+            
+        case .start:
+            
+            //append attachment string to full string
+            fullString.append(attachmentString)
+            
+            //append title string to full string
+            fullString.append(title)
+            
+            //assign range for image
+            range = NSMakeRange(0, 2)
+            
+            fullString.addAttribute(NSForegroundColorAttributeName, value: imageColor, range: range)
+            return fullString
+
+        case .end:
+            
+            //append attachment string to full string
+            fullString.append(attachmentString)
+            
+            title.append(fullString)
+            
+            //assign range for image
+            range = NSMakeRange(text.characters.count, 1)
+            
+            title.addAttribute(NSForegroundColorAttributeName, value: imageColor, range: range)
+            return title
+
+        }
+        
+    }
+}
--- TintColorStringWIthImage/ViewController.swift
+++ TintColorStringWIthImage/ViewController.swift
@@ -8,76 +8,27 @@
 
 import UIKit
 
-enum ImagePosition {
-    case start, end
-}
-
 class ViewController: UIViewController {
     
     let label = UILabel(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: 300, height: 300)))
-    let attachment = NSTextAttachment()
-    let imageColor = UIColor.orange
-    let stringColor = UIColor.green
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         createLabel()
 
-        createAttributedString(imagePosition: .start, text: " Hello This is string with image", image: UIImage(named: "swift_logo")!)
+        let formattedString = ImageUtility.createAttributedString(text: " Hello This is string with image", image: UIImage(named: "swift_logo")!, imagePos: .start, imageColor: .green)
         
-        //createAttributedString(imagePosition: .end, text: "Hello This is string with image ", image: UIImage(named: "swift_logo")!)
+        //let formattedString = ImageUtility.createAttributedString(text: "Hello This is string with image", image: UIImage(named: "swift_logo")!, imagePos: .end, imageColor: .cyan  )
+        label.attributedText = formattedString
+
     }
     
     func createLabel() {
         label.numberOfLines = 0
         view.addSubview(label)
         label.center = view.center
-        label.textColor = stringColor
-    }
-    
-    func createAttributedString(imagePosition: ImagePosition, text: String, image: UIImage) {
-        
-        var range = NSMakeRange(0, 0)
-        attachment.bounds = CGRect(origin: CGPoint(x: 0, y: -2), size: CGSize(width: 15, height: 15))
-
-        // image must be set to Template rendering mode
-        attachment.image = image
-        
-        //create a string with image attachment
-        let attachmentString = NSAttributedString(attachment: attachment)
-        
-        //create a title attributed string
-        let title = NSAttributedString(string: text)
-        
-        // Need to use a space, or this bug/feature isn't activated
-        let fullString = NSMutableAttributedString(string: " ")
-        
-        switch imagePosition {
-            
-        case .start:
-            //append attachment string to full string
-            fullString.append(attachmentString)
-            
-            //append title string to full string
-            fullString.append(title)
-            
-            //assign range for image
-            range = NSMakeRange(0, 2)
-        case .end:
-            
-            //append title string to full string
-            fullString.append(title)
-            
-            //append attachment string to full string
-            fullString.append(attachmentString)
-            
-            //assign range for image
-            range = NSMakeRange(0, 1)
-        }
-        
-        fullString.addAttribute(NSForegroundColorAttributeName, value: imageColor, range: range)
-        label.attributedText = fullString
+        label.textColor = UIColor.blue
     }
     
     override func didReceiveMemoryWarning() {
